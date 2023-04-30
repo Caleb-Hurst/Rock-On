4c35aeda3834a72a57e4363dd4077fff57df4ad8
Caleb-Hurst <hursty.caleb@gmail.com>
2023-04-30 15:03:51 -0500

    asdf
    asdf

---
+++ src/main/java/com/RockOn/RockOnApplication.java
@@ -0,0 +1,13 @@
+package com.RockOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockOnApplication.class, args);
+	}
+
+}
