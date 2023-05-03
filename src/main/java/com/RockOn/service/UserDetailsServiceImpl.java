9659da8f9ccbd8395090135912700a8355647f3c
Caleb-Hurst <hursty.caleb@gmail.com>
2023-05-03 15:08:12 -0500

    asdf
    asdf

---
+++ src/main/java/com/RockOn/service/UserDetailsServiceImpl.java
@@ -0,0 +1,18 @@
+package com.RockOn.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+ 
