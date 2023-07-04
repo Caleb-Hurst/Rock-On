4a7799d6334af770f6c049d593ea534e0a382d81
Caleb Hurst <114947204+Caleb-Hurst@users.noreply.github.com>
2023-07-04 13:56:14 -0500

    Revert "Deploy"

--- src/main/java/com/RockOn/security/SecurityConfiguration.java
+++ src/main/java/com/RockOn/security/SecurityConfiguration.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,22 +16,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 //@EnableGlobalAuthentication
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   
-//  @Autowired
-//  private PasswordEncoder passwordEncoder;
+  @Autowired
+  private PasswordEncoder passwordEncoder;
   @Autowired
   private UserDetailsService userDetailsService;
   
   @Bean
-  public BCryptPasswordEncoder passwordEncoder() {
+  public PasswordEncoder passwordEncoder () {
     return new BCryptPasswordEncoder();
   }
-
+  
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth
       .userDetailsService(userDetailsService)
-      .passwordEncoder(passwordEncoder());
-//    auth.inMemoryAuthentication().withUser("caleb").password("password123").roles("USER","ADMIN");
+      .passwordEncoder(passwordEncoder);
+//	  auth.inMemoryAuthentication().withUser("caleb").password("password123").roles("USER","ADMIN");
   }
   
   @Override
