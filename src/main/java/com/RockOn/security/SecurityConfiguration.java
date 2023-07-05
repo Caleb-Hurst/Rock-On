7a33a3e261ad655cb487538d348c75587ac7d92e
Caleb-Hurst <hursty.caleb@gmail.com>
2023-07-05 11:49:08 -0500

    test

--- src/main/java/com/RockOn/security/SecurityConfiguration.java
+++ src/main/java/com/RockOn/security/SecurityConfiguration.java
@@ -4,19 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-//@EnableGlobalAuthentication
+@EnableGlobalAuthentication
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   
 //  @Autowired
 //  private PasswordEncoder passwordEncoder;
-  @Autowired
+ 
+	@Autowired
   private UserDetailsService userDetailsService;
   
   @Bean
